256c76f1cfcb37d9e515619b5099405acf0a0581
Long Nguyen <81816026+Anieast@users.noreply.github.com>
2023-04-10 14:18:27 +0700

    Add files via upload

---
+++ telegram.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# Push Notification (using Telegram)
+#
+# Script Name   : check_mk_telegram-notify.sh
+# Description   : Send Check_MK notifications by Telegram
+# Author        : https://github.com/filipnet/checkmk-telegram-notify
+# License       : BSD 3-Clause "New" or "Revised" License
+# ======================================================================================
+
+# Telegram API Token
+# Find telegram bot named "@botfather", type /mybots, select your bot and select "API Token" to see your current token
+if [ -z ${NOTIFY_PARAMETER_1} ]; then
+        echo "No Telegram token ID provided. Exiting" >&2
+        exit 2
+else
+        TOKEN="${NOTIFY_PARAMETER_1}"
+fi
+
+# Telegram Chat-ID or Group-ID
+# Open "https://api.telegram.org/bot<YOUR_TOKEN>/getUpdates" inside your Browser and send a HELLO to your bot, refresh side
+if [ -z ${NOTIFY_PARAMETER_2} ]; then
+        echo "No Telegram Chat-ID or Group-ID provided. Exiting" >&2
+        exit 2
+else
+        CHAT_ID="${NOTIFY_PARAMETER_2}"
+fi
+
+# Privacy settings to anonymize/masking IP addresses
+if [[ ${NOTIFY_PARAMETER_3} == "privacy" ]]; then
+        # IPv4 IP addresses
+        if [ ${NOTIFY_HOST_ADDRESS_4} ]; then
+                slice="${NOTIFY_HOST_ADDRESS_4}"
+                count=1
+                while [ "$count" -le 4 ]
+                do
+                        declare sec"$count"="${slice%%.*}"
+                        slice="${slice#*.}"
+                        count=$((count+1))
+                done
+                # Adjust the output to your privacy needs here (Details in the readme.md)
+                NOTIFY_HOST_ADDRESS_4="${sec1}.${sec2}.2.${sec4}"
+        fi
+
+        # IPv6 IP addresses
+        if [ ${NOTIFY_HOST_ADDRESS_6} ]; then
+                slice="${NOTIFY_HOST_ADDRESS_6}"
+                count=1
+                while [ "$count" -le 8 ]
+                do
+                        declare sec"$count"="${slice%%:*}"
+                        slice="${slice#*:}"
+                        count=$((count+1))
+                done
+                # Adjust the output to your privacy needs here (Details in the readme.md)
+                NOTIFY_HOST_ADDRESS_6="${sec1}:${sec2}:${sec3}:${sec4}:ffff:ffff:ffff:${sec8}"
+        fi
+else
+        echo "Invalid privacy parameter, check your Check_MK settings." >&2
+fi
+
+# Set an appropriate emoji for the current state
+# Feel free to change the emoji to your own taste. This is done by customizing the UTF8 code. Examples here: https://apps.timwhitlock.info/emoji/tables/unicode
+if [[ ${NOTIFY_WHAT} == "SERVICE" ]]; then
+        STATE="${NOTIFY_SERVICESHORTSTATE}"
+else
+        STATE="${NOTIFY_HOSTSHORTSTATE}"
+fi
+case "${STATE}" in
+    OK|UP)
+        EMOJI=$'\xE2\x9C\x85' # white heavy check mark
+        ;;
+    WARN)
+        EMOJI=$'\xE2\x9A\xA0' # warning sign
+        ;;
+    CRIT|DOWN)
+        EMOJI=$'\xF0\x9F\x86\x98' # squared sos
+        ;;
+    UNKN)
+        EMOJI=$'\xF0\x9F\x94\x84' # anticlockwise downwards and upwards open circle arrows
+        ;;
+esac
+
+# Create a MESSAGE variable to send to your Telegram bot
+MESSAGE=""
+if [[ ${NOTIFY_WHAT} == "SERVICE" ]]; then
+	MESSAGE+="${EMOJI} ***${NOTIFY_SERVICEDESC}*** ${EMOJI}%0A%0A"
+	MESSAGE+="***Check_MK: ${NOTIFY_HOSTNAME}/${NOTIFY_SERVICEDESC}***%0A"
+	MESSAGE+="\`\`\`%0A"
+	MESSAGE+="Host:     ${NOTIFY_HOSTNAME}%0A"
+	MESSAGE+="Address:  ${NOTIFY_HOST_ADDRESS_4}%0A"
+	MESSAGE+="Service:  ${NOTIFY_SERVICEDESC}%0A"
+	MESSAGE+="Event:    ${NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE} -> ${NOTIFY_SERVICESHORTSTATE}%0A"
+	MESSAGE+="Output:   ${NOTIFY_SERVICEOUTPUT}%0A"
+	MESSAGE+="\`\`\`"
+else
+	MESSAGE+="${EMOJI} ***${NOTIFY_HOSTNAME}*** ${EMOJI}%0A%0A"
+	MESSAGE+="***Check_MK: ${NOTIFY_HOSTNAME}***%0A"
+	MESSAGE+="\`\`\`%0A"
+	MESSAGE+="Host:     ${NOTIFY_HOSTNAME}%0A"
+	MESSAGE+="Address:  ${NOTIFY_HOST_ADDRESS_4}%0A"
+	MESSAGE+="Event:    ${NOTIFY_PREVIOUSHOSTHARDSHORTSTATE} -> ${NOTIFY_HOSTSHORTSTATE}%0A"
+	MESSAGE+="Output:   ${NOTIFY_HOSTOUTPUT}%0A"
+	MESSAGE+="\`\`\`"
+fi
+
+# Send message to Telegram bot
+curl -S \
+     -X POST \
+     -d chat_id="${CHAT_ID}" \
+     -d text="${MESSAGE}" \
+     -d disable_notification=true \
+     -d parse_mode="markdown" \
+     "https://api.telegram.org/bot${TOKEN}/sendMessage"
+if [ $? -ne 0 ]; then
+        echo "Not able to send Telegram message" >&2
+        exit 2
+else
+        exit 0
+fi
